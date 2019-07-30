8c907f11f2150078676566acc59338136be578eb
Anzhela <zyzzza94@gmail.com>
2019-07-30 18:43:33 +0300

    adjust the height of the image and text

--- js/slider.js
+++ js/slider.js
@@ -4,8 +4,8 @@ $('.popular-goods-items').slick({
 	// autoplay: true,
 	arrows: true,
 	// autoplaySpeed: 2000,
-	prevArrow: '<button class="btn-popular btn-popular__left" type="button"><</button>',
-	nextArrow: '<button class="btn-popular btn-popular__right" type="button">></button>'
+	prevArrow: '<button class="btn-popular btn-popular__left" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>',
+	nextArrow: '<button class="btn-popular btn-popular__right" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>'
 });
 
 
@@ -13,6 +13,6 @@ $('.discounted-goods-items').slick({
 	slidesToShow: 4,
 	slidesToScroll: 1,
 	arrows: true,
-	prevArrow: '<button class="btn-discounted btn-discounted__left" type="button"><</button>',
-	nextArrow: '<button class="btn-discounted btn-discounted__right" type="button">></button>'
+	prevArrow: '<button class="btn-discounted btn-discounted__left" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>',
+	nextArrow: '<button class="btn-discounted btn-discounted__right" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>'
 });
