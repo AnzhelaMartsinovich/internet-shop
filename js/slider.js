a314c31094e9bdb46b31f27b8e21a524323d76b0
Anzhela <zyzzza94@gmail.com>
2019-08-02 01:42:49 +0300

    slider

--- js/slider.js
+++ js/slider.js
@@ -1,9 +1,18 @@
-$('.popular-goods-items').slick({
-	slidesToShow: 4,
+$('.main-slider-items').slick({
+	slidesToShow: 1,
 	slidesToScroll: 1,
+	dots: true,
 	// autoplay: true,
 	arrows: true,
 	// autoplaySpeed: 2000,
+	prevArrow: '<button class="btn-main-slider btn-main-slider__left" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>',
+	nextArrow: '<button class="btn-main-slider btn-main-slider__right" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>'
+});
+
+$('.popular-goods-items').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	arrows: true,
 	prevArrow: '<button class="btn-popular btn-popular__left" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>',
 	nextArrow: '<button class="btn-popular btn-popular__right" type="button"><img src="./img/icon/arrow_slider.svg" alt=""></button>'
 });
